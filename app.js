37f7e694ddc607f482a87bbd32e603212cfc7197
Aswin110 <aswinashok110@gmail.com>
2024-02-15 18:30:57 +0530

    added a log

--- app.js
+++ app.js
@@ -19,9 +19,8 @@ var app = express();
 
 mongoose.set("strictQuery", false);
 
-// Define the database URL to connect to.
 const mongoDB = process.env.MONGODB_URI;
-// console.log(mongoDB);
+console.log(mongoDB);
 main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect(mongoDB);
